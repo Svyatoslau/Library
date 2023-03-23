8a4e444a7d7a8589383e1680ec2112d2c26e0796
Svyatoslav <svyatyar2@gmail.com>
2023-03-23 23:28:07 +0300

    [ADD] interfaces for services

---
+++ Library.Core/Services/IService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Core.Services;
+public interface IService<T> where T : class
+{
+    Task<T> AddAsync(T entity);
+    Task<T> UpdateAsync(T entity);
+    Task<T> DeleteAsync(T entity);
+    Task<T> GetByIdAsync(int id);
+    Task<IEnumerable<T>> GetAllAsync();
+}
---
+++ Library.Core/Services/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using Library.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Core.Services;
+public interface IUserService : IService<User>
+{
+    Task<string> Authenticate(string nickname, string password);
+    Task<User> Registr(string nickname, string password);
+}
