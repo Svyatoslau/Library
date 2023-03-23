cd137a94ee25774719c84f8bda41879786b5a475
Svyatoslav <svyatyar2@gmail.com>
2023-03-23 17:46:40 +0300

    [ADD] endpoints

---
+++ Library.API/Controllers/BookController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.API.Controllers;
+[Authorize]
+[Route("api")]
+[ApiController]
+public class BookController : ControllerBase
+{
+    
+    public BookController()
+    {
+
+    }
+
+    [HttpGet("books")]
+    public async Task<ActionResult> GetBooksAsync()
+    {
+        return Ok();
+    }
+
+    [HttpGet("book/{id}")]
+    public async Task<ActionResult> GetBookByIdAsync(
+        [FromRoute] int id
+    )
+    {
+        return Ok();
+    }
+
+    [HttpGet("book")]
+    public async Task<ActionResult> GetBookByIsbnAsync(
+        [FromQuery] string isbn
+    )
+    {
+        return Ok();
+    }
+
+    [HttpPost("book")]
+    public async Task<ActionResult> CreateBookAsync(
+        [FromBody] string book
+    )
+    {
+        return Ok();
+    }
+
+    [HttpPut("book")]
+    public async Task<ActionResult> UpdateBookAsync(
+        [FromBody] string book
+    )
+    {
+        return Ok();
+    }
+
+    [HttpDelete("book/{id}")]
+    public async Task<ActionResult> DeleteBookAsyncNow(
+        [FromRoute] int id
+    )
+    {
+        return Ok();
+    }
+
+}
--- Library.API/Controllers/UserController.cs
+++ Library.API/Controllers/UserController.cs
@@ -2,8 +2,21 @@
 using Microsoft.AspNetCore.Mvc;
 
 namespace Library.API.Controllers;
-[Route("api/[controller]")]
+[Route("api/user")]
 [ApiController]
 public class UserController : ControllerBase
 {
+    public UserController() { }
+
+    [HttpPost("login")]
+    public async Task<ActionResult> LoginUserAsync()
+    {
+        return Ok();
+    }
+
+    [HttpPost("registration")]
+    public async Task<ActionResult> RegistrUserAsync() 
+    {
+        return Ok();
+    }
 }
